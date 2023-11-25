6c0ec0f3041e49752f0cfa8c939e00a62dfe4c7b
Danilo Scipioni <daniloedu@gmail.com>
2023-11-25 11:26:35 -0300

    finish configuration jest with next

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { 
-}
-
-module.exports = nextConfig
+module.exports = {
+    compiler: {
+      relay: {
+        // This should match relay.config.js
+        src: './',
+        artifactDirectory: './__generated__',
+        language: 'typescript',
+        eagerEsModules: false,
+      },
+    },
+  }
