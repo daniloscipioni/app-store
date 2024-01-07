e522fc0eb40677d6eef89fb3488d4b1dd6d393fd
Danilo Scipioni <daniloedu@gmail.com>
2024-01-07 10:09:01 -0300

    Fix test

--- pages/orders/tests/list.test.js
+++ pages/orders/tests/list.test.js
@@ -11,6 +11,6 @@ console.log(myMock1.mock.instances);
 describe('List of orders',  () => {
   test('Is a list of orders', async () => {
     var orders = await OrderSvc.getOrders();
-      expect(orders.data[0].Id).toBe("6543f288fb287140f51474d8");
+      expect(orders.data[0].Id).toBe("6547f7060e76ef08cc95066c");
   });
 });
